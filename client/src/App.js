a74b953f75ae063a0d4a239e0ddd1fd5a5d13318
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-13 21:42:12 +0800

    Routing components
    Routing Layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,16 @@ import './App.css';
 import Post from "./Post";
 import Header from "./Header";
 import {Route, Routes} from "react-router-dom";
+import Layout from './Layout';
 
 function App() {
   return (
     <Routes>
-      <Route index element={
-     <main>
-      <Header />       
-        <Post />
-        <Post />
-        <Post />
-     </main>
-    } /> 
-    </Routes>
+     <Route path="/" element={<Layout />}>
+      <Route index element={<Post />} /> 
+      <Route path={'/login'} element={<div>login page</div>} />
+     </Route>
+    </ Routes>
   );
 }
 
