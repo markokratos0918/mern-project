9cb0af494130db140d6901173739a8c5d62d7085
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-13 20:50:58 +0800

    Creating client component

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Post from "./Post";
+import Header from "./Header";
+import {Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={
+     <main>
+      <Header />       
+        <Post />
+        <Post />
+        <Post />
+     </main>
+    } /> 
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/src/Post.js
@@ -0,0 +1,19 @@
+export default function Post() {
+    return(
+    <div className="post">
+    <div className="image">
+    <img src="https://techcrunch.com/wp-content/uploads/2023/02/a.jpeg?w=1390&crop=1" alt=""/>
+    </div>
+    <div className="texts">
+    <h2>The Arcimoto Fun Utility Vehicle is a blast (that might not last)</h2>
+    <p className="info">
+      <a className="author">David Beck</a>
+      <time>2023-02-10 14:45</time>
+    </p>
+    <p className="summary">That doesn’t look safe.” The statement would follow me for days. Every time I mentioned I was test driving Arcimoto’s Fun Utility Vehicle — an open air, all-electric three-wheeler — a friend or co-worker would pipe up to state, what to them, seemed like the obvious.
+      After all, most cars have four wheels, not three. They also tend to have doors and airbags too. 
+    </p>
+    </div>
+  </div>   
+  );
+}
