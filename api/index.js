003933146b345819fed1621e81036cc1642cb148
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-15 00:25:57 +0800

    Setting up database
    Mondodb using mongoose

--- api/index.js
+++ api/index.js
@@ -14,3 +14,5 @@ app.post('/register', (req,res) =>{
 
 // deport port for express
 app.listen(4000);
+//mongodb+srv://markde:0GZr06eFjtlOLMrn@cluster0.6f3vbcy.mongodb.net/?retryWrites=true&w=majority
+//0GZr06eFjtlOLMrn
