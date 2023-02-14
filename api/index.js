fd2123882f26b5e1c9bbb2c9e6a249c06bfec737
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-15 00:38:34 +0800

    Fixed deprecationwarning Mongoose
    fixed by adding mongoose.set('strictQuery', true);

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
 const app = express();
 
 app.use(cors());
 //json parser//
 app.use(express.json()); 
+
+mongoose.connect('mongodb+srv://markde:0GZr06eFjtlOLMrn@cluster0.6f3vbcy.mongodb.net/?retryWrites=true&w=majority');
 
 app.post('/register', (req,res) =>{
     const {username,password} = req.body;
@@ -12,7 +16,7 @@ app.post('/register', (req,res) =>{
 });
 
 
-// deport port for express
+// default port for express
 app.listen(4000);
 //mongodb+srv://markde:0GZr06eFjtlOLMrn@cluster0.6f3vbcy.mongodb.net/?retryWrites=true&w=majority
 //0GZr06eFjtlOLMrn
