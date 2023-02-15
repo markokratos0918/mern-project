8f4a9236a1212009d05c8031c461369624a98594
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-15 21:57:53 +0800

    Updating register using express
    adding catch
    bcrypt password

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 const User = require('./models/User')
+const bcrypt = require('bcryptjs');
 const app = express();
 
 app.use(cors());
@@ -10,9 +11,14 @@ app.use(express.json());
 
 mongoose.connect('mongodb+srv://markde:0GZr06eFjtlOLMrn@cluster0.6f3vbcy.mongodb.net/?retryWrites=true&w=majority');
 
-app.post('/register', (req,res) =>{
+app.post('/register', async (req,res) => {
     const {username,password} = req.body;
-    res.json({requestData:{username,password}});
+    try{
+    const userDoc = await User.create({username, password});
+    res.json(userDoc);
+    }catch(e) {
+        res.status(400).json(e);
+    }
 });
 
 
--- client/src/Header.js
+++ client/src/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <header>
-          <Link to="" className="logo">MyBlog</Link>
+          <Link to="" className="logo">FarmKnowledge</Link>
           <nav>
           <Link to="/login">Login</Link>
           <Link to="/register">Register</Link>
