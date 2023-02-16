224b84fe16b99819d6f4db2e45015f6070f0c912
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-16 23:18:48 +0800

    Added user context log out function

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,15 +1,16 @@
 import {Link} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
+import {UserContext} from "./UserContext";
 
 
 export default function Header() {
-  const [username,setUsername] = useState(null);
+  const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect (() => {
     fetch('http://localhost:4000/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
-        setUsername(userInfo.username);
+        setUserInfo(userInfo);
       });
     });
   }, []);
@@ -19,8 +20,11 @@ function logout() {
     credentials: 'include',
     method: 'POST',
   });
-  setUsername(null);
+  setUserInfo(null);
 }
+
+const username = userInfo?.username;
+
     return (
         <header>
           <Link to="/" className="logo">FarmKnowledge</Link>
