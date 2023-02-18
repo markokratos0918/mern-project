9f2bdd75f5684aaa0662756259e5a80ac6bdaa5d
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-18 21:03:16 +0800

    Adding PostPage

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,16 +1,22 @@
 import {format} from "date-fns";
-export default function Post({title,summary,cover, content,createdAt, author}) {
+import { Link } from "react-router-dom";
+
+export default function Post({_id,title,summary,cover, content,createdAt, author}) {
     return(
     <div className="post">
-    <div className="image">
-    <img src={'http://localhost:4000/'+cover} alt=""/> 
-    </div>
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+        <img src={'http://localhost:4000/'+cover} alt=""/> 
+        </Link>
+      </div>
     <div className="texts">
-    <h2>{title}</h2>
-    <p className="info">
-      <a className="author">{author.username}</a>
-      <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time> 
-    </p>
+      <Link to={`/post/${_id}`}>
+      <h2>{title}</h2>
+      </Link>
+        <p className="info">
+        <a className="author">{author.username}</a>
+        <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time> 
+        </p>
     <p className="summary">{summary}</p>
     </div>
   </div>   
