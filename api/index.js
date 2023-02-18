ce1941e8df81e520c75c448c6a3cc5c1f32a7b0e
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-18 14:14:17 +0800

    Set up fs to rename file upload

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //bridge to mongodb//
 mongoose.set('strictQuery', true);
 const User = require('./models/User')
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs'); //encryp password//
 const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
-const uploadMidleware = multer({ dest: 'uploads/' })
-
+const uploadMidleware = multer({ dest: 'uploads/' }) //to save file in upload folder//
+const fs = require('fs'); //renaming file//
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfee123123434678090ouojljl';
@@ -19,7 +19,7 @@ app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json()); 
 app.use(cookieParser());
 
-mongoose.connect('mongodb+srv://markde:0GZr06eFjtlOLMrn@cluster0.6f3vbcy.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://markde:0GZr06eFjtlOLMrn@cluster0.6f3vbcy.mongodb.net/?retryWrites=true&w=majority');//key connecting to db//
 
 // registering user
 app.post('/register', async (req,res) => {
@@ -54,6 +54,7 @@ app.post('/login', async (req,res) => {
     }
 });
 
+//change profile when logged in//
 app.get('/profile', (req,res) => {
     const {token} = req.cookies;
     jwt.verify(token, secret, {}, (err, info)=> {
@@ -66,11 +67,13 @@ app.post('/logout', (req,res) => {
     res.cookie('token', '').json('ok');
 });
 
-app.post('/post', uploadMidleware.single('file'), (req,res) => {
-    const {originalname} = req.file;
-    const parts = originalname.split('.');
-    const ext = parts[parts.lenght -1];
-    res.json({files:req.file});
+//create post//
+app.post('/post', uploadMidleware.single('file'), (req,res) => { 
+    const {originalname,path} = req.file; //grab file//
+    const parts = originalname.split('.'); //capture end of file name//
+    const ext = parts[parts.length -1]; 
+    fs.renameSync(path, path+'.'+ext); //rename file upload + extension//
+    res.json({ext});
 });
 
 // default port for express
